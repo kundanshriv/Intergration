605574558a29a38ccd4171790624595ad98d5dda
kundan_shriv <kundan_shriv@yahoo.com>
2018-06-08 17:24:35 +0530

    commit by kundan1

---
+++ BuidManagement/src/git/Integration.java
@@ -0,0 +1,11 @@
+package git;
+
+public class Integration {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Eclipse github and jenkins integration change1");
+
+	}
+
+}
